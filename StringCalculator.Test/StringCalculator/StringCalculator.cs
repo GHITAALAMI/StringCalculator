d9f7a31badf711dcffde4d11f42e879b3d58be20
GHITAALAMI <122487118+GHITAALAMI@users.noreply.github.com>
2024-03-07 12:02:07 +0100

    refactoring for negative numbers

--- StringCalculator.Test/StringCalculator/StringCalculator.cs
+++ StringCalculator.Test/StringCalculator/StringCalculator.cs
@@ -1,4 +1,7 @@
-﻿namespace StringCalculator
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace StringCalculator
 {
     public class Calculator
     {
@@ -7,14 +10,46 @@
             string[] strings = numbers.Split(',','\n');
 
             int sum = 0;
+            List<int> numberlist = new List<int>();
+
             foreach (var s in strings)
             {
                 if (int.TryParse(s, out int number))
                 {
-                    sum += number;
+                    numberlist.Add(number); 
                 }
             }
-            return sum;
+
+            CheckForNegativeNumbers(numberlist);
+            return numberlist.Sum();
     }
+        private static void CheckForNegativeNumbers(List<int> numberlist)
+        {
+            var negativenumbrs = numberlist.Where(X => X < 0).ToList();
+            if (negativenumbrs.Any())
+            {
+                throw new NegativesNotAllowedException();
+            }
+        }
+    }
+
+    [Serializable]
+    internal class NegativesNotAllowedException : Exception
+    {
+        public NegativesNotAllowedException()
+        {
+        }
+
+        public NegativesNotAllowedException(string? message) : base(message)
+        {
+        }
+
+        public NegativesNotAllowedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected NegativesNotAllowedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
